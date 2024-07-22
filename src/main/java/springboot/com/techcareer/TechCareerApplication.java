62aefbf17e72a685f0fa8309efa5ec6242722564
EmineOzbek <emine.ozbek.55@gmail.com>
2024-07-22 23:16:26 +0300

    add second week codes

---
+++ src/main/java/springboot/com/techcareer/TechCareerApplication.java
@@ -0,0 +1,13 @@
+package springboot.com.techcareer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCareerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechCareerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springboot/com/techcareer/TechCareerApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.com.techcareer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechCareerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
